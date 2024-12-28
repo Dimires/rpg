7e29c2a0a93f118a5dc207a34e7e2338296918da
Dimires <150380816+Dimires@users.noreply.github.com>
2024-12-28 19:59:49 +0300

    Починил отправку фото

--- photosk.py
+++ photosk.py
@@ -1,12 +1,13 @@
-# Путь к файлам
-photo_1 = r'data\5289688684351842566.jpg'
-photo_2 = r'data\5289649879322325464.jpg'
-photo_3 = r'data\5289688684351842575.jpg'
-photo_4 = r'data\5291937928659985906.jpg'
-photo_5 = r'data\5289686128846300379.jpg'
-photo_6 = r'data\5291937928659984905.jpg'
-photo_7 = r'data\5291937928659985095.jpg'
-photo_8 = r'data\5291937928659985145.jpg'
-photo_9 = r'data\5289649879322326237.jpg'
-photo_10 = r'data\5291937928659985718.jpg'
-photo_11 = r'data\5291937928659985519.jpg'
+import os
+
+photo_1 = os.path.join('data', '5289688684351842566.jpg')
+photo_2 = os.path.join('data', '5289649879322325464.jpg')
+photo_3 = os.path.join('data', '5289688684351842575.jpg')
+photo_4 = os.path.join('data', '5291937928659985906.jpg')
+photo_5 = os.path.join('data', '5289686128846300379.jpg')
+photo_6 = os.path.join('data', '5291937928659984905.jpg')
+photo_7 = os.path.join('data', '5291937928659985095.jpg')
+photo_8 = os.path.join('data', '5291937928659985145.jpg')
+photo_9 = os.path.join('data', '5289649879322326237.jpg')
+photo_10 = os.path.join('data', '5291937928659985718.jpg')
+photo_11 = os.path.join('data', '5291937928659985519.jpg')
